9e119ab341481bf2f57989ac80fa1cc702ede4a8
clayton <clayton@laptop2>
2019-07-09 15:28:14 -0400

    added baasic heap functiobnality(not implemeneted) and moved agloritmhs out of main

---
+++ headers/maxHeap.h
@@ -0,0 +1,6 @@
+#include "../src/C_algorithm.h"
+void __sift_down(size_t size,char* start,char* end,size_t node,size_t len,algo_predicate comp,algo_function move);
+void __sift_up(size_t size,char* start,char* end,size_t node,size_t len,algo_predicate comp,algo_function move);
+void heapify(size_t size,char* start,char* end,algo_predicate comp,algo_function move);
+void push_heap(size_t size,char* start,char* end,algo_predicate pred,algo_function move);
+void pop_heap(size_t size,char* start,char* end,algo_predicate comp,algo_function move);
